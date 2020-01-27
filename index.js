7a186f2ee63f5d932dc492ed88af232f65ea333e
João Vitor <joaovitorcoelho13@gmail.com>
2020-01-27 08:58:55 -0300

    chore:Middlewares global

--- index.js
+++ index.js
@@ -5,16 +5,24 @@ server.use(express.json());
 
 const users = ['Joao', 'Vitor'];
 
+server.use((request, response, next)=>{
+    console.time('request')
+    console.log(`Metodo: ${request.method}, URL: ${request.url};`);
+
+    next();
+
+    console.timeEnd('request');
+});
+
 server.get('/users/:index',(request, response)=>{
     const {index} = request.params;
 
     return response.json({messagem:`Buscando usuario ${users[index]}`});
-
 });
 
 server.get('/users',(request, response)=>{
     return response.json(users);
-})
+});
 
 
 server.post('/users',(request, response)=>{
