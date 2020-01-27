1c8a77baf8756856f3ac9c9b47210cfaab704747
João Vitor <joaovitorcoelho13@gmail.com>
2020-01-27 08:48:37 -0300

    chore:CRUD

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require('express');
+
+const server = express();
+server.use(express.json());
+
+const users = ['Joao', 'Vitor'];
+
+server.get('/users/:index',(request, response)=>{
+    const {index} = request.params;
+
+    return response.json({messagem:`Buscando usuario ${users[index]}`});
+
+});
+
+server.get('/users',(request, response)=>{
+    return response.json(users);
+})
+
+
+server.post('/users',(request, response)=>{
+    
+    const {nome} = request.body;
+    users.push(nome);
+
+    return response.json(users);
+});
+
+server.put('/users/:index',(request, response)=>{
+    
+    const {nome} = request.body;
+    const {index} = request.params;
+
+    users[index]=nome;
+
+    return response.json(users);
+});
+
+server.delete('/users/:index',(request, response)=>{
+    
+    const {index} = request.params;
+
+    users.splice(index,1);
+
+    return response.send();
+});
+
+
+server.listen(3000);
